92c14ac8691bcdff7bc4891677bfaf727ea35f12
yoonseo94 <o0o0526@naver.com>
2022-05-10 00:00:35 +0900

    hello-git project init!
    세부내용1
    세부내용2
    세부내용3

---
+++ hello-git/src/com/kh/git/Hellowrold.java
@@ -0,0 +1,19 @@
+package com.kh.git;
+/**
+ * SCM software configuration Management
+ * - 코드추적(버전관리)외에 build, packaging, deploy등 프로젝트관련 프로세스를 관리
+ *
+ * VCS Version Control System
+ * - 프로젝트 작성/수정/보완에 따른 변경내역을 버전으로 관리
+ * 
+ * 버전관리 프로그램
+ * - server-client : 중앙저장소를 공유한 각각의 클라이언트(개발자)에서 저장소의 일부만 가져와서 작업후, 중앙저장소에 반영
+ * 		- subversion
+ * - 분산형 : 참여하는 각각의 클라이언트에서 전체저장소에 대한 복제본을 가지고 작업.
+ * 		- git
+ */
+public class Hellowrold {
+	public static void main(String[] args) {
+		
+	}
+}
