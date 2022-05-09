5354c1247b3fa5d27eacef824fb0552ebf2a812e
Yoonseo Choi <98252597+yoonseo94@users.noreply.github.com>
2022-05-10 00:51:50 +0900

    Cat#age추가

--- hello-git/src/com/kh/animal/Cat.java
+++ hello-git/src/com/kh/animal/Cat.java
@@ -5,6 +5,7 @@ public class Cat {
 	private String name; //master에서 추가
 	private String color; //animal에서 추가
 	private String breed; //master에서 작업
+	private int age; // github에서 작업
 	public void cry() {
 		
 		System.out.println("야옹~");
