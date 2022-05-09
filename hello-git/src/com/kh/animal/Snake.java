e7d0f455b5b64f1b98056226a9cf29bcaadd424c
yoonseo94 <o0o0526@naver.com>
2022-05-10 00:19:46 +0900

    Snake#length추가

--- hello-git/src/com/kh/animal/Snake.java
+++ hello-git/src/com/kh/animal/Snake.java
@@ -1,8 +1,9 @@
 package com.kh.animal;
 
 public class Snake {
-
+	private double length;// master에 추가
 	private String name; // master에서 추가
+	
 	public void move() {
 		System.out.println("스르르륵~");
 	}
